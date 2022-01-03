8eb79d6d6e157408c771cbb5405e5441555c8aaa
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-01-03 21:35:05 +0000

    Formatted stats figures

--- js/script.js
+++ js/script.js
@@ -21,22 +21,37 @@ data.forEach(function (country) {
     '</option>';
   countryData[country.countryInfo.iso2] = country;
 });
-
+var num;
+function numFormatter(num) {
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(0) + 'k'; // convert to k for number from > 1000 < 1 million
+  } else if (num > 1000000) {
+    return (num / 1000000).toFixed(0) + 'm'; // convert to m for number from > 1 million
+  } else if (num < 900) {
+    return num; // if value < 1000, nothing to do
+  }
+}
 function onCountryChange(name) {
   var countryCode = name.options[name.selectedIndex].value;
 
   var selectedCountryData = countryData[countryCode];
 
   selectedCountry.innerHTML = selectedCountryData.country;
-  casesToday.innerHTML = selectedCountryData.todayCases;
-  casesTotal.innerHTML = selectedCountryData.cases;
-  casesPerMillionPop.innerHTML = selectedCountryData.casesPerOneMillion;
-  recoveredToday.innerHTML = selectedCountryData.todayRecovered;
-  recoveredTotal.innerHTML = selectedCountryData.recovered;
-  recoveredPerMillionPop.innerHTML = selectedCountryData.recoveredPerOneMillion;
-  deathsToday.innerHTML = selectedCountryData.todayDeaths;
-  deathsTotal.innerHTML = selectedCountryData.deaths;
-  deathsPerMillionPop.innerHTML = selectedCountryData.deathsPerOneMillion;
+  casesToday.innerHTML = numFormatter(selectedCountryData.todayCases);
+  casesTotal.innerHTML = numFormatter(selectedCountryData.cases);
+  casesPerMillionPop.innerHTML = numFormatter(
+    selectedCountryData.casesPerOneMillion
+  );
+  recoveredToday.innerHTML = numFormatter(selectedCountryData.todayRecovered);
+  recoveredTotal.innerHTML = numFormatter(selectedCountryData.recovered);
+  recoveredPerMillionPop.innerHTML = numFormatter(
+    selectedCountryData.recoveredPerOneMillion
+  );
+  deathsToday.innerHTML = numFormatter(selectedCountryData.todayDeaths);
+  deathsTotal.innerHTML = numFormatter(selectedCountryData.deaths);
+  deathsPerMillionPop.innerHTML = numFormatter(
+    selectedCountryData.deathsPerOneMillion
+  );
 
   console.log(selectedCountryData.cases);
 }
